03a326b99a8647849ef6583bc6a3cfc619d56145
Abhishek Konduri <abhishek.konduri@sjsu.edu>
2018-12-13 03:38:28 -0800

    Output Commented for Maps

--- golabs/golang/basics/src/app/09.maps.go
+++ golabs/golang/basics/src/app/09.maps.go
@@ -17,7 +17,7 @@ func main() {
 
     // To create an empty map, use the builtin `make`:
     // `make(map[key-type]val-type)`.
-    m := make( map [string] int )
+	m := make( map [string] int )                                 
 
     // Set key/value pairs using typical `name[key] = val`
     // syntax.
@@ -26,11 +26,11 @@ func main() {
 
     // Printing a map with e.g. `Println` will show all of
     // its key/value pairs.
-    fmt.Println("map:", m)
+    fmt.Println("map:", m)                                           //Output: map1: map[k1:7 k2:13]
 
     // Get a value for a key with `name[key]`.
     v1 := m["k1"]
-    fmt.Println("v1: ", v1)
+    fmt.Println("v1: ", v1)                                          //OP : v1:  7
 
     // The builtin `len` returns the number of key/value
     // pairs when called on a map.
@@ -49,12 +49,12 @@ func main() {
     // itself, so we ignored it with the _blank identifier_
     // `_`.
     _, prs := m["k2"]
-    fmt.Println("prs:", prs)
+    fmt.Println("prs:", prs                                         //OP : prs: true
 
     // You can also declare and initialize a new map in
     // the same line with this syntax.
     n := map [string] int {"foo": 1, "bar": 2}
-    fmt.Println("map:", n)
+    fmt.Println("map:", n)                                          //OP : map: map[bar:2 foo:1]
 }
 
 
